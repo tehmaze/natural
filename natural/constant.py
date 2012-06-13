6229c6429e800e427c1db6e570014f68155dc2cf
Wijnand Modderman-Lenstra <wijnand.modderman@booking.com>
2012-06-13 18:26:49 +0200

    More docs

--- natural/constant.py
+++ natural/constant.py
@@ -1,6 +1,50 @@
 import gettext
 import locale
 import os
+
+
+'''
+.. py:attribute:: LOCALE_PATH
+
+   Path on the file system to the locale/ directory, used to find translations
+   for the current locale.
+
+
+.. py:attribute:: CONVENTION
+
+   Container for all the locale conventions, see
+   http://docs.python.org/library/locale.html#locale.localeconv
+
+
+.. py:attribute:: ORDINAL_SUFFIX
+
+   Suffixes for the ordinal number representation.
+
+
+.. py:attribute:: LARGE_NUMBER_SUFFIX
+
+   Suffixes for the word number representation.
+
+
+.. py:attribute:: FILESIZE_SUFFIX
+
+   Suffixes for the file size representations.
+
+
+.. py:function:: _(message)
+
+   Return the localised translation of ``message``, based on the current global
+   domain, language, and locale directory.
+
+
+.. py:function:: _multi(singular, plural, count)
+
+   Return the localised translation based on ``count``, the ``singular``
+   version if ``count == 1``, the ``plural`` version otherwise.
+
+'''
+
+
 locale.setlocale(locale.LC_ALL, '')
 
 LOCALE_PATH = os.path.join(os.path.dirname(__file__), 'locale')
