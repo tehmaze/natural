56ad1b118d3f3d384f53ec5b5d95da90ce818373
Wijnand Modderman-Lenstra <wijnand.modderman@booking.com>
2012-06-14 17:48:44 +0200

    Enable doctest and intersphinx extensions

--- docs/conf.py
+++ docs/conf.py
@@ -27,7 +27,8 @@ locale.setlocale(locale.LC_ALL, 'en_US')
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.todo', 'sphinx.ext.coverage']
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.todo', 'sphinx.ext.coverage',
+    'sphinx.ext.doctest', 'sphinx.ext.intersphinx']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
