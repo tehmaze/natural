e14e8938f7e0482a9d78156a6f7d26d309844c54
Wijnand Modderman-Lenstra <maze@pyth0n.org>
2013-01-09 22:11:29 +0100

    Bump version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='natural',
-    version='0.1',
+    version='0.1.1',
     description='Convert data to their natural (human-readable) format',
     long_description='''
 Example Usage
