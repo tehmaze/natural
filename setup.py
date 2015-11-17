5b0ba6e0f1fd58cd279e33b070d73c60f6ee9484
William Grzybowski <wg@FreeBSD.org>
2015-11-17 15:20:13 -0200

    Lets call it 0.1.6

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ except:
     from distutils.core import setup
 
 setup(name='natural',
-    version='0.1.5',
+    version='0.1.6',
     description='Convert data to their natural (human-readable) format',
     long_description='''
 Example Usage
